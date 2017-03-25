7861889af347c31c8a94b85bb473096480361e0b
rhairy <randall.harrison91@gmail.com>
2017-03-25 09:08:20 -0700

    Changed example to use unsigned pointer.

--- pthreads/hello.c
+++ pthreads/hello.c
@@ -5,8 +5,9 @@
 #define NUM_THREADS 10
 
 void *hello(void *id)
-{
-	printf("Hello from thread: %i\n", (unsigned) id);
+{	
+	unsigned *my_id = (unsigned *) id;
+	printf("Hello from thread: %i\n", *my_id);
 	pthread_exit(NULL);
 }
 
@@ -15,12 +16,14 @@ int main(int argc, char* argv[])
 	pthread_t threads[NUM_THREADS];
 	int res;
 	unsigned i;
-	unsigned *pld;
+	unsigned *j;
+	*j = 0;
 	for (i = 0; i < NUM_THREADS; i++) {
-		res = pthread_create(&threads[i], NULL, hello, (void *) i);
+		res = pthread_create(&threads[i], NULL, hello, (void *) j);
 		if (res) {
 			perror("pthread_create()");
 		}
+		*j = *j + 1;
 	}
 	pthread_exit(NULL);
 	return 0;
